12c22e65492ec92f55f1fe7b95bfc21d9856d235
qp <dima@home.se>
2018-02-07 05:53:04 +0100

    chapter11new.html

---
+++ js/js.js
@@ -0,0 +1,27 @@
+$(document).on('click', '.divTableCell', function() { //для подсвечивания классов и выведение списка циферок
+  $('.divTableCell:not(this)').css("background-color", "");
+  $(this).css("background-color", "#ff9625");
+
+  var show = $(this).data('show');
+  $(show).removeClass("hide").siblings().addClass("hide");
+});
+
+$(document).on('click', '.circle', function() {    //кружочки подсветка вывод
+  $('.circle:not(this)').css("background-color", "");
+  $(this).css("background-color", "#ff9625");
+
+    var show = $(this).data('show');
+    $(show).removeClass("hide").siblings().addClass("hide");
+
+});
+
+
+
+// навигация вкл выкл
+function openNav() {
+  document.getElementById("navigation").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("navigation").style.width = "0%";
+}
