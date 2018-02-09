1e657097d3da036f33cc9f795e4566390a33c104
Qpheliax <36218500+Qpheliax@users.noreply.github.com>
2018-02-09 11:44:47 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -31,3 +31,7 @@ function openNav() {
 function closeNav() {
   document.getElementById("navigation").style.width = "0%";
 }
+
+$(document).ready(function(){
+    $( "#head" ).load( "header.html" );
+   });
